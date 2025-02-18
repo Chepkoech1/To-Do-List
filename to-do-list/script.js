2f362e70df0629bbd7d7e05b7a3853dda053820b
Sandra <sandrachepkoech60@gmail.com>
2025-02-18 16:38:38 +0300

    Update script.js

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -22,3 +22,33 @@ function toggleTaskCompletion(index){
     tasks[index].completed=!tasks[index].completed;
     renderTasks();
 }
+function  renderTasks(){
+    const taskList=document.getElementById('task-list');
+    taskList.innerHTML='';
+
+    tasks.forEach((task, index)=>{
+        const li=document.createElement('li');
+
+        if (task.completed) {
+            li.classList.add('completed')
+        }
+        const taskText =document.createElement('span');
+        taskText.textContent=task.name;
+        li.appendChild(taskText);
+
+        //added a delee button
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = '×';
+        deleteBtn.classList.add('delete-btn');
+        deleteBtn.onclick = () => deleteTask(index);
+        li.appendChild(deleteBtn);
+        
+        // Add Task Toggle Button
+        li.onclick = () => toggleTaskCompletion(index);
+        
+        taskList.appendChild(li);
+
+    });
+
+    }
+
